c65e4747f866d8853296e3016d4d45a25f5e275b
Cillian Smith <cilliansmith.1010@gmail.com>
2025-08-22 21:55:50 +0100

    add diff-so-fancy and change core editor to vim

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -6,7 +6,11 @@
 }: {
   programs.git = {
     enable = true;
+    diff-so-fancy.enable = true;
     userName = "smithc";
     userEmail = "cilliansmith.1010@gmail.com";
+    extraConfig = {
+      core.editor = "nvim";
+    };
   };
 }
